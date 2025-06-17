0dfc43ed037c6f529c5428f840a01cc41586ac6f
AlCpwnd <alexis.dejaeger@eurelec-trading.eu>
2025-06-17 16:40:21 +0200

    Code optimisations:
    > Starts with the "General" channel
    > Skips the channel loop if only a single Teams channel is found
    > Rewrote the file naming / export code

--- MGraph/TeamsStructure.ps1
+++ MGraph/TeamsStructure.ps1
@@ -45,22 +45,36 @@ $Report = foreach($Team in $Teams){
     $Channels = Get-MgTeamChannel -TeamId $Team.Id
     $j = 0
     $jMax = $Channels.Count
-    foreach($Channel in $Channels){
-        if($jMax -gt 5){
-            # To prevent the progress bar from flickering.
-            $j ++
-            Write-Progress -Activity "Documenting Channels [$j/$jMax]" -Status $Channel.DisplayName -Id 1 -PercentComplete (($j/$jMax)*100) -ParentId 0
+    $general = $Channels[$Channels.DisplayName.IndexOf('General')]
+    [TeamsPermission]::new(
+        $Team.Id,
+        $Team.DisplayName,
+        $Team.Visibility,
+        $general.Id,
+        $general.DisplayName,
+        $general.MembershipType
+    )
+    if($jMax -gt 1){
+        foreach($Channel in $Channels){
+            if($jMax -gt 5){
+                # To prevent the progress bar from flickering.
+                $j ++
+                Write-Progress -Activity "Documenting Channels [$j/$jMax]" -Status $Channel.DisplayName -Id 1 -PercentComplete (($j/$jMax)*100) -ParentId 0
+            }
+            if($Channel.DisplayName -eq 'General'){
+                continue
+            }
+            [TeamsPermission]::new(
+                $Team.Id,
+                $Team.DisplayName,
+                $Team.Visibility,
+                $Channel.Id,
+                $Channel.DisplayName,
+                $Channel.MembershipType
+            )
         }
-        [TeamsPermission]::new(
-            $Team.Id,
-            $Team.DisplayName,
-            $Team.Visibility,
-            $Channel.Id,
-            $Channel.DisplayName,
-            $Channel.MembershipType
-        )
+        Write-Progress -Activity "Documenting Channels [$j/$jMax]" -Status $Channel.DisplayName -Id 1 -ParentId 0 -Completed
     }
-    Write-Progress -Activity "Documenting Channels [$j/$jMax]" -Status $Channel.DisplayName -Id 1 -ParentId 0 -Completed
 }
 Write-Progress -Activity "Documenting Teams [$i/$iMax]" -Status $Team.DisplayName -Id 0 -Completed
 
@@ -68,7 +82,11 @@ Write-Progress -Activity "Documenting Teams [$i/$iMax]" -Status $Team.DisplayNam
 $Path = "$PSScriptRoot\$(Get-Date -Format yyyyMMdd)_TeamsStructure.csv" 
 $i = 1
 while(Test-Path -Path $Path){
-    $Path = $Path.Replace(".csv"," ($i).csv")
+    if($path -match '\(\d+\).csv'){
+        $path = $Path.Replace("$($i-1)).csv","$i).csv")
+    }else{
+        $Path = $Path.Replace(".csv"," ($i).csv")
+    }
     $i ++
 }
 $Report | Export-Csv -Path $Path -Encoding UTF8 -NoTypeInformation
